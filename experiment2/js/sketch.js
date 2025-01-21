f0cdac5ef11e0aaab4b7a49af5171e8f8737fc73
MarvelMcD <155485365+MarvelMcD@users.noreply.github.com>
2025-01-21 15:01:42 -0800

    trails

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -7,13 +7,15 @@
 let canvasContainer;
 var centerHorz, centerVert;
 var speed;
+var balls = [];
+var originalX = -1;
+var originalY = -1;
 
 function resizeScreen() {
   centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
   centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
   console.log("Resizing...");
   resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
 }
 
 // setup() function is called once when the program starts
@@ -22,9 +24,10 @@ function setup() {
   canvasContainer = $("#canvas-container");
   let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
   canvas.parent("canvas-container");
-  // resize canvas if the page is resized
+
+  // Create a speed slider
   speed = createSlider(0, 2, 0.5, 0.01);
-  speed.position(0, 0);
+  speed.position(10, 10);  // Adjust the position of the slider on the screen
 
   $(window).resize(function() {
     resizeScreen();
@@ -42,15 +45,31 @@ class Ball {
     this.bounciness = bounciness;
     this.gravity = gravity;
     this.color = color(255);  // Default color is white
+    this.trail = [];  // Array to store previous positions of the ball
   }
 
   draw() {
     noStroke();
+    
+    // Draw the trail first, fading the previous positions
+    for (let i = 0; i < this.trail.length; i++) {
+      let alpha = map(i, 0, this.trail.length, 255, 0); // Decrease alpha for fading effect
+      fill(this.color.levels[0], this.color.levels[1], this.color.levels[2], alpha);
+      ellipse(this.trail[i].x, this.trail[i].y, 10);
+    }
+    
+    // Draw the ball on top of the trail
     fill(this.color);
     circle(this.ballX, this.ballY, 10);
   }
 
   tick(time = 1) {
+    // Save the current position in the trail array
+    this.trail.push(createVector(this.ballX, this.ballY));
+    if (this.trail.length > 10) { // Limit the length of the trail
+      this.trail.shift(); // Remove the oldest position
+    }
+
     // Move Ball
     this.ballX += this.speedX * time;
     this.ballY += this.speedY * time;
@@ -102,39 +121,37 @@ class Ball {
   }
 }
 
-var balls = [];
-
-var originalX = -1;
-var originalY = -1;
-
 function mousePressed() {
   originalX = mouseX;
   originalY = mouseY;
-  describe("A new ball is being created.");
+  console.log("A new ball is being created.");
 }
 
 function mouseReleased() {
   balls.push(new Ball(originalX, originalY, mouseX - originalX, mouseY - originalY, 0.05, 0.8, 1));
   originalX = -1;
   originalY = -1;
-  describe("A new ball was created at (${mouseX},${mouseY})");
+  console.log(`A new ball was created at (${mouseX}, ${mouseY})`);
 }
 
 function draw() {
-  background("black");
+  background(0);  // Clear the canvas with a black background
+  
+  // Draw a line while the mouse is pressed, indicating the direction and speed of the ball
   if (originalX != -1 && originalY != -1) {
-    stroke("white");
+    stroke(255);
     strokeWeight(1);
     line(originalX, originalY, mouseX, mouseY);
     strokeWeight(5);
     point(originalX, originalY);
-    describe("There are ${balls.length} balls bouncing in a box. A new ball is being created with a speed of (${mouseX - originalX}, ${mouseY - originalY}).");
+    console.log(`There are ${balls.length} balls bouncing. A new ball is being created with speed (${mouseX - originalX}, ${mouseY - originalY}).`);
   } else {
-    describe("There are ${balls.length} balls bouncing in a box.");
+    console.log(`There are ${balls.length} balls bouncing.`);
   }
   
-  for (var ball in balls) {
-    balls[ball].draw();
-    balls[ball].tick(speed.value());
+  // Update and draw all balls
+  for (var ball of balls) {
+    ball.draw();
+    ball.tick(speed.value());
   }
 }
