e2d1c429e6cf9ab547cf5ee5e0fd162f90cbef7b
MarvelMcD <MarvelMcD@gmail.com>
2025-03-05 22:45:24 -0800

    added extra words

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -9,23 +9,11 @@
 // In a longer project I like to put these in a separate file
 
 const fillers = {
-  /*
-  adventurer: ["My dude", "Bro", "WesBot", "Adventurer", "Traveller", "Fellow", "Citizen", "Ashen One", "Dragonborn", "Cool person", "Tarnished", "勇者", "$adventurer and $adventurer", "$adventurer, $adventurer, and $adventurer", "Geoff"],
-  pre: ["Fra", "Tro", "Gre", "Pan", "Ast", "Ara"],
-  post: ["gria", "ston", "gott","-on-the-lee", "ora", "Ara", "uwu"],
-  people: ["kindly", "meek", "brave", "wise", "sacred", "cherished", "honored", "forgotten", "apathetic", "mystic", "orca", "帥氣"],
-  item: ["axe", "staff", "book", "cloak", "shield", "club", "sword", "magic gloves", "galvel", "fists", "mace", "potato"],
-  num: ["two", "three", "eleven", "so many", "too many", "an unsatisfying number of", "barely any", "an unspecified amount of", "surely a satisfactory number of"],
-  looty: ["gleaming", "valuable", "esteemed", "rare", "exalted", "scintillating", "kinda gross but still usefull", "complete garbage"],
-  loots: ["coins", "chalices", "ingots", "hides", "victory points", "gems","scrolls", "bananas", "noodles", "goblins", "CS Majors", "college credits"],
-  baddies: ["orcs", "glubs", "fishmen", "cordungles", "mountain trolls", "college professors", "dragon", "evil $adventurer", "agents of chaos"],
-  message: ["call", "txt", "post", "decree", "shoutz", "tweets", "choiche", "hearkens", "harkening", "harkenening", "harkenenening", "...wait, no! Come back", "Watermelon"],
-  */
-  gamer: ["Gamer", "Dude", "broski", "Burger", "Gabagool", "Silver", "My guy", "Champ"],
+  gamer: ["Gamer", "Dude", "broski", "Burger", "Gabagool", "Silver", "My guy", "Champ", "NVM","Prod"],
   site: ["A", "B"],
-  econ: ["eco", "full buy", "force pistols", "zeco", "Hero buy"],
+  econ: ["eco", "full buy", "force pistols", "zeco", "Hero buy", , "Broken Buy"],
   strat: ["rush no stop", "slow default", "default", "walk out", "run boost", "let them dump utility"],
-  legend: ["Olofmeister", "Shroud", "Aleksib", "S1mple", "donk", "ZywOo", "ropz", "NiKo", "XANTARES", "device", "coldzera", "kennyS", "Twistzz", "Stewie2K", "EliGE", "FalleN", "GeT_RiGhT", "tarik"],
+  legend: ["Olofmeister", "Shroud", "Aleksib", "S1mple", "donk", "ZywOo", "ropz", "NiKo", "XANTARES", "device", "coldzera", "kennyS", "Twistzz", "Stewie2K", "EliGE", "FalleN", "GeT_RiGhT", "Tarik", "Donk"],
   item: ["Ak", "M4", "Deagle", "Smoke", "p250", "zeus", "flashbang", "Mac10"],
   win: ["dub", "win", "victory", "cheese", "epic no.1 victory", "bread", "game over with", "round", "dubskii", "pogchamp"],
   motto: ["pull through", "focus up", "lock in", "drink the gatorade", "steamroll", "poop on the other team", "start poopin", "clutch up"],
@@ -34,15 +22,6 @@ const fillers = {
 };
 
 
-/*
-
-const template = `$adventurer, heed my $message!
-
-I have just come from $pre$post where the $people folk are in desperate need. Their town has been overrun by $baddies. You must venture forth at once, taking my $item, and help them.
-
-It is told that the one who can rescue them will be awarded with $num $looty $loots. Surely this must tempt one such as yourself!
-`;
-*/
 
 const template = `$gamer, it's time to get this $win!
 
@@ -50,7 +29,6 @@ Here's the strat that will win us this CS game, all you need to do is channel yo
 I can drop you a $item before the round starts. This game has $match yet, I know we're going to $motto and get this $win. $gamer are you with me? 
 `;
 
-// STUDENTS: You don't need to edit code below this line.
 
 const slotPattern = /\$(\w+)/;
 
